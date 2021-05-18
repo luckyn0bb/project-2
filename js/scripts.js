8d029aff9c5b11a358a355ae6409b5961a30d068
luckyn0bb <84342923+luckyn0bb@users.noreply.github.com>
2021-05-19 02:44:58 +0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -255,7 +255,7 @@ function initMap() {
 }
 
 function initBBSRMap() {
-    var la_fiesta = {lat: 20.305826, lng: 85.85480189999998};
+    var la_fiesta = {lat: -7.27799850449979, lng: 112.73563081078825};
     var map = new google.maps.Map(document.getElementById('map-canvas'), {
         zoom: 15,
         center: la_fiesta,

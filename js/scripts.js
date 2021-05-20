6ae19b44d17b9238a5e8dc75174d6e6a47f90d62
luckyn0bb <84342923+luckyn0bb@users.noreply.github.com>
2021-05-21 01:32:56 +0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -214,8 +214,8 @@ $(document).ready(function () {
 
         $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
 
-        if (MD5($('#invite_code').val()) !== 'b0e53b10c1f55ede516b240036b88f40'
-            && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
+        if (MD5($('#invite_code').val()) !== 'e10adc3949ba59abbe56e057f20f883e'
+            && MD5($('#invite_code').val()) !== 'e10adc3949ba59abbe56e057f20f883e') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
             $.post('https://script.google.com/macros/s/AKfycbxKXyoDmN8LoHXt2xnin4lbJ3OZw3h0t0rJGzUXHvGXfbo-RKS6/exec', data)
